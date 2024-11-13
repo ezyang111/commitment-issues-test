0b16d2a35cd97e44dfc1a750291fd97ff579f852
edwardyeung04 <edwardbuddyboy@gmail.com>
2024-11-12 22:21:41 -0800

    feature | backend: Implement function to send welcome email to new users

---
+++ backend/feature.py
@@ -0,0 +1,7 @@
+def get_user_profile(user_id):
+    # existing code
+
+#def send_welcome_email(user_id):
+    # New feature to send an email to a new user
+    #user = get_user_profile(user_id)
+    #email.send(user.email, "Welcome to the platform!")
